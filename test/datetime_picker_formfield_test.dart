c707219b6be8ad9c71035cfe5654f4f6341fab87
Jacob Phillips <jifalops@gmail.com>
2019-05-29 13:50:05 -0400

    remove erroneous import

--- test/datetime_picker_formfield_test.dart
+++ test/datetime_picker_formfield_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
-import 'package:datetime_picker_formfield/time_picker_formfield.dart';
 
 void main() {
   final dateFormat = DateFormat("EEEE, MMMM d, yyyy 'at' h:mma");
